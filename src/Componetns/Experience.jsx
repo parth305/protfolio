757e6573ea63215c7cc7cf4eaa5d59c436b5dc50
parth <parth305pandya@gmail.com>
2022-07-12 20:20:04 +0530

    some change

--- src/Componetns/Experience.jsx
+++ src/Componetns/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
     <div id="experience" className="bg-[#4e8ff1]">
         <h1 className="text-2xl text-center text-black">Experience</h1>
         <div className='flex justify-center py-5 flex-col align-middle'>
-          <div className='mx-auto w-[35%]'>
+          <div className='mx-auto md:w-[35%] w-11/12'>
             <h1 className='text-2xl'>Web Developer</h1>
           <h2 className='text-xl font-bold'>Whatcode - an it service based company</h2>
           <small >June 2022 to Present</small>
