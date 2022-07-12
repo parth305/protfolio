18225755b265a0942591f2b7fd6c856c577dcdca
parth <parth305pandya@gmail.com>
2022-07-12 20:46:43 +0530

    minor changes

--- src/Componetns/Experience.jsx
+++ src/Componetns/Experience.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div id="experience" className="bg-[#4e8ff1]">
+    <div id="experience" className="bg-[#5090f0]">
         <h1 className="text-2xl text-center text-black">Experience</h1>
         <div className='flex justify-center py-5 flex-col align-middle'>
           <div className='mx-auto md:w-[35%] w-11/12'>
-            <h1 className='text-2xl'>Web Developer</h1>
+            <h1 className='text-2xl'>Full stack Web Developer - intern</h1>
           <h2 className='text-xl font-bold'>Whatcode - an it service based company</h2>
           <small >June 2022 to Present</small>
           <p className='font-normal'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum unde vel nisi at animi aliquam sunt facere sapiente minima porro.</p>
